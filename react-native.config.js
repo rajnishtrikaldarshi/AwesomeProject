d52011e815577ef5675fe77c86ca885ca43cf8cf
rajnishtrikaldarshi <rajnish8singh9@gmail.com>
2024-12-12 15:35:42 +0530

    Implement the maps

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,37 @@
 module.exports = {
-    assets: ['./node_modules/react-native-vector-icons/Fonts'],
-  };
+  assets: ['./node_modules/react-native-vector-icons/Fonts'],
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: [
+        'AIRMap',
+        'AIRMapCallout',
+        'AIRMapCalloutSubview',
+        'AIRMapCircle',
+        'AIRMapHeatmap',
+        'AIRMapLocalTile',
+        'AIRMapMarker',
+        'AIRMapOverlay',
+        'AIRMapPolygon',
+        'AIRMapPolyline',
+        'AIRMapUrlTile',
+        'AIRMapWMSTile',
+      ],
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: [
+        'AIRMap',
+        'AIRMapCallout',
+        'AIRMapCalloutSubview',
+        'AIRMapCircle',
+        'AIRMapHeatmap',
+        'AIRMapLocalTile',
+        'AIRMapMarker',
+        'AIRMapOverlay',
+        'AIRMapPolygon',
+        'AIRMapPolyline',
+        'AIRMapUrlTile',
+        'AIRMapWMSTile',
+      ],
+    },
+  },
+};
